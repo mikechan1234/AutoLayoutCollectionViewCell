de7fd63b1e808eb1a470816e32d75f6163294a00
Michael Chan <kwochan@ebay.com>
2017-07-04 12:44:17 +0800

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Chan, Michael(AWF) on 04/07/2017.
+//  Copyright © 2017 eBay Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
